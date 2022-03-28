5a63f61007637a74cee68e04cfc61a05fc7b5c76
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-03-28 10:17:17 -0300

    Método de inserir incluído

--- lista_simples.cpp
+++ lista_simples.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 typedef struct no {
@@ -23,15 +25,41 @@ int main(){
 }
 
 Celula *inserir(int valor, Celula *lista){
+    Celula *novo, *p, *pR;
+    
     //alocar memória
-
+    novo = (Celula *)malloc(sizeof(Celula));
+    
     //depositar valor
+    novo->dado = valor;
+    novo->prox = NULL;
+
+    //testar se é primeira vez
+    if (!lista) return novo;
 
     //localizar posição de inserção
+    for (pR = NULL, p = lista; p; pR = p, p = p->prox){
+        if (valor < p->dado){
+            break;
+        }
+    }
 
     //"engatar"
 
+    if (p == lista){
+        //testar se no início
+        novo->prox = lista;
+        return novo;
+    }else if (!p){
+        //testar se no fim
+        pR->prox = novo;
+    }else{
+       pR->prox = novo;
+       novo->prox = p;  
+    }
+
     //retornar o início da lista
+    return lista;
 }
 
 void exibir_lista(Celula *lista){
